7408f55d44cfe9993e659dd031c667c4c2402be0
Ryan M <129496054+Syyren@users.noreply.github.com>
2024-12-04 18:07:57 -0700

    implemented footer

--- data_analysis_project/data_analysis_app/views.py
+++ data_analysis_project/data_analysis_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 from .meme import forg
 
 from django.http import HttpResponse
+
 def frog(request):
-    return HttpResponse(forg())
+    return render(request, "forg.html", {"name" : "Steven", "forg" : mark_safe(forg())})
