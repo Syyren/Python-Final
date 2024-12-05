311b80e82fa814734ddae6b207bfc34f728a4bf5
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-12-05 00:00:39 -0700

    Created ERD and associated models

--- data_analysis_project/data_analysis_app/models.py
+++ data_analysis_project/data_analysis_app/models.py
@@ -1,40 +1,36 @@
 from django.db import models
-from datetime import date
-from .qol import str_to_list, str_to_date, convert_duration
 
-class Title():
-    def __init__(self, id : str,
-                 type : str,
-                 title : str, 
-                 director: str,
-                 cast: str,
-                 country: str,
-                 date_added : str,
-                 release_year: int,
-                 rating: str,
-                 duration: str,
-                 listed_in: list,
-                 description: str):
-        self.__id : str = id
-        self.type = type
-        self.title : str = title
-        self.director : list = str_to_list(str(director))
-        self.cast : list = str_to_list(str(cast))
-        self.country : list = str_to_list(str(country))
-        self.date_added : date = str_to_date(str(date_added))
-        self.release_year : int = release_year
-        self.rating : str = rating
-        self.duration : int = convert_duration(str(duration))
-        self.listed_in : list = str_to_list(str(listed_in))
-        self.description : str = description
+class Title(models.Model):
+    id = models.CharField(max_length=10, primary_key=True) #good to show_id: s999999999
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    date_added = models.DateField(blank=True, null=True)
+    release_year = models.IntegerField(blank=True, null=True)
+    rating = models.CharField(max_length=10, blank=True, null=True)
+    duration = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
-    def getDurationAsString(self):
-        duration_string = f"{self.duration}"
-        s = ""
-        if self.duration > 1:
-            s = "s"
-        if self.type == "Movie":
-            duration_string += f" min{s}"
-        elif self.type == "TV Show":
-            duration_string += f" Season{s}"
-        return duration_string
+class Director(models.Model):
+    id = models.AutoField(primary_key=True)
+    director_name = models.CharField(max_length=255)
+
+class Cast(models.Model):
+    id = models.AutoField(primary_key=True)
+    cast_name = models.CharField(max_length=255)
+
+class ListedIn(models.Model):
+    id = models.AutoField(primary_key=True)
+    listed_in_name = models.CharField(max_length=255)
+
+class TitleDirector(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE)
+
+class TitleCast(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    cast = models.ForeignKey(Cast, on_delete=models.CASCADE)
+
+class TitleListedIn(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+    listed_in = models.ForeignKey(ListedIn, on_delete=models.CASCADE)
