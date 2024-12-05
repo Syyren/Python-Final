2a72ececcef5b968bc7e1114d507bc9fae65db57
Ryan M <129496054+Syyren@users.noreply.github.com>
2024-12-05 01:04:51 -0700

    improved forg

--- data_analysis_project/data_analysis_app/meme.py
+++ data_analysis_project/data_analysis_app/meme.py
@@ -1,7 +1,7 @@
 #you didn't see this one
 
-def forg():
+def forg(exponent : int = 10):
     string = "<img src='https://media.tenor.com/tS4120QBuugAAAAj/frog-spin-frog.gif'></img>"
-    for i in range(10):
+    for i in range(exponent):
         string += string
     return string
