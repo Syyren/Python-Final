736a02ec0738af34ad3b9d0afba6b686892292ff
Ryan M <129496054+Syyren@users.noreply.github.com>
2024-12-05 10:53:32 -0700

    migrated process_data function to qol

--- data_analysis_project/data_analysis_app/urls.py
+++ data_analysis_project/data_analysis_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 #declaring accessible URLs and their paths
 urlpatterns = [
-path('forg/', views.frog),
-path('titles/',views.view_titles, name='view_titles'),
+    path('', views.index),
+    path('titles/',views.view_titles, name='view_titles'),
+    path('forg/', views.frog)
 ]
