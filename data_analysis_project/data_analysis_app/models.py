5fac407e5b5ce9b5280daa47397fb5b26a46f42d
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-12-05 01:18:28 -0700

    Added related values from intermediary tables to Titles, as they were missed... oops

--- data_analysis_project/data_analysis_app/models.py
+++ data_analysis_project/data_analysis_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-class Title(models.Model):
-    id = models.CharField(max_length=10, primary_key=True) #good to show_id: s999999999
-    type = models.CharField(max_length=50)
-    title = models.CharField(max_length=255, blank=True, null=True)
-    country = models.CharField(max_length=255, blank=True, null=True)
-    date_added = models.DateField(blank=True, null=True)
-    release_year = models.IntegerField(blank=True, null=True)
-    rating = models.CharField(max_length=10, blank=True, null=True)
-    duration = models.IntegerField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
-
+#Secondary Tables
 class Director(models.Model):
     id = models.AutoField(primary_key=True)
     director_name = models.CharField(max_length=255)
@@ -23,14 +13,33 @@ class ListedIn(models.Model):
     id = models.AutoField(primary_key=True)
     listed_in_name = models.CharField(max_length=255)
 
-class TitleDirector(models.Model):
-    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+#Primary Table
+class Title(models.Model):
+    id = models.CharField(max_length=10, primary_key=True) #good to show_id: s999999999
+    type = models.CharField(max_length=50)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    country = models.CharField(max_length=255, blank=True, null=True)
+    date_added = models.DateField(blank=True, null=True)
+    release_year = models.IntegerField(blank=True, null=True)
+    rating = models.CharField(max_length=10, blank=True, null=True)
+    duration = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    #Handling Many to Many Relationships
+    directors = models.ManyToManyField(Director, through='ShowsDirectors')
+    casts = models.ManyToManyField(Cast, through='ShowsCasts')
+    listed_in = models.ManyToManyField(ListedIn, through='ShowsListedIns')
+
+
+#Intermediary Tables
+class TitlesDirectors(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.CASCADE) #CAscade delete for reducing redundancy
     director = models.ForeignKey(Director, on_delete=models.CASCADE)
 
-class TitleCast(models.Model):
+class TitlesCasts(models.Model):
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
     cast = models.ForeignKey(Cast, on_delete=models.CASCADE)
 
-class TitleListedIn(models.Model):
+class TitlesListedIns(models.Model):
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
     listed_in = models.ForeignKey(ListedIn, on_delete=models.CASCADE)
